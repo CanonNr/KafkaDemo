07eaee95eaae21fdf8804ed063f7dc6ba78ed68d
Canon <717437709@qq.com>
2021-03-08 16:52:41 +0800

    动态监听

--- consumer-service/src/main/java/cn/lksun/kafkademo/consumerservice/task/ConsumerListener.java
+++ consumer-service/src/main/java/cn/lksun/kafkademo/consumerservice/task/ConsumerListener.java
@@ -1,16 +1,34 @@
 package cn.lksun.kafkademo.consumerservice.task;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.TopicPartition;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class ConsumerListener {
 
-    @KafkaListener(topics = "test")
-    public void onMessage(String message){
-        log.info("message:{} ",message);
+    @Value("${server.port}")
+    String port;
+
+    @Value("${spring.kafka.consumer.group-id}")
+    String groupId;
+
+    @Value("${test.partition-id}")
+    String partitionId;
+
+//    @KafkaListener(topics = "test01")
+//    @KafkaListener(topics = "#{'${spring.kafka.topics}'.split('\\\\ ')}")
+    @KafkaListener(
+        topicPartitions ={
+            @TopicPartition(topic = "test01", partitions = {"#{${test.partition-id}}"})
+        }
+    )
+    public void onMessage(String message) throws InterruptedException {
+        log.info("message[{}]  :  {} ,{}",port,message,partitionId);
+        Thread.sleep(100);
     }
 
 }
